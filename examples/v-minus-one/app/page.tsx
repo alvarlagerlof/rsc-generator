f57964012f663b6dd3983d3407ea288ab060ca5b
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2024-05-22 01:23:03 +0200

    Integrate rsc-parser and add dark mode

--- examples/v-minus-one/app/page.tsx
+++ examples/v-minus-one/app/page.tsx
@@ -16,6 +16,7 @@ import { ErrorBoundary } from "react-error-boundary";
 // import { unstable_Viewer, unstable_createFlightResponse } from "@rsc-parser/core";
 // @ts-expect-error TODO: Fix later
 import { BeatLoading } from "respinner";
+import { unstable_Viewer as Viewer } from "@rsc-parser/core";
 
 export const maxDuration = 240;
 
@@ -81,7 +82,7 @@ export default function Page() {
                 }}
               >
                 <div
-                  className={`rounded-lg bg-white h-28 block p-3 ${
+                  className={`rounded-lg bg-white dark:bg-slate-900 h-28 block p-3 ${
                     version.id === currentVersionId
                       ? "outline outline-2 outline-black"
                       : ""
@@ -90,11 +91,11 @@ export default function Page() {
                   {version.isPending ? (
                     <div className="flex flex-col justify-between size-full items-center">
                       <div className="w-full h-1/6 flex flex-row justify-between">
-                        <div className="bg-gray-100 animate-pulse rounded-md w-1/5 h-full" />
-                        <div className="bg-gray-100 animate-pulse rounded-md w-1/3 h-full" />
+                        <div className="bg-gray-100 dark:bg-slate-800 animate-pulse rounded-md w-1/5 h-full" />
+                        <div className="bg-gray-100 dark:bg-slate-800 animate-pulse rounded-md w-1/3 h-full" />
                       </div>
-                      <div className="bg-gray-100 animate-pulse rounded-md w-2/3 h-1/2" />
-                      <div className="bg-gray-100 animate-pulse rounded-md w-full h-1/6" />
+                      <div className="bg-gray-100 dark:bg-slate-800 animate-pulse rounded-md w-2/3 h-1/2" />
+                      <div className="bg-gray-100 dark:bg-slate-800 animate-pulse rounded-md w-full h-1/6" />
                     </div>
                   ) : (
                     <ShrinkPreview>
@@ -120,7 +121,7 @@ export default function Page() {
         {state === "edit" ? (
           <>
             <div className="flex gap-8 flex-col grow">
-              <div className="overflow-y-auto max-h-[500px] min-h-96 bg-white rounded-lg p-4">
+              <div className="overflow-y-auto max-h-[500px] min-h-96 bg-white dark:text-black dark:bg-slate-900 rounded-lg p-4">
                 <ErrorBoundary
                   fallback={<p>Error</p>}
                   key={currentVersion?.rscPayload ?? ""}
@@ -133,9 +134,64 @@ export default function Page() {
                 </ErrorBoundary>
               </div>
 
-              <Box title="RSC Payload:">
-                <RawRscPayload rscPayload={currentVersion?.rscPayload ?? ""} />
-              </Box>
+              {typeof currentVersion?.rscPayload === "string" &&
+              currentVersion?.rscPayload !== "" ? (
+                <>
+                  <div className="flex flex-col gap-3">
+                    <Box title="RSC Payload (parsed):">
+                      <Viewer
+                        payload={
+                          getValidRscPayloadFromPartial(
+                            currentVersion?.rscPayload ?? ""
+                          ) ?? ""
+                        }
+                      />
+                    </Box>
+                    <a
+                      href="https://github.com/alvarlagerlof/rsc-parser"
+                      className="flex flex-row gap-2 items-center hover:underline"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16px"
+                        height="16px"
+                        viewBox="0 0 20 20"
+                        version="1.1"
+                      >
+                        <title>github [#142]</title>
+                        <desc>Created with Sketch.</desc>
+                        <defs></defs>
+                        <g
+                          id="Page-1"
+                          stroke="none"
+                          strokeWidth="1"
+                          fill="none"
+                          fillRule="evenodd"
+                        >
+                          <g
+                            transform="translate(-140.000000, -7559.000000)"
+                            fill="currentColor"
+                          >
+                            <g
+                              id="icons"
+                              transform="translate(56.000000, 160.000000)"
+                            >
+                              <path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"></path>
+                            </g>
+                          </g>
+                        </g>
+                      </svg>
+                      <span>rsc-parser on GitHub</span>
+                    </a>
+                  </div>
+
+                  <Box title="RSC Payload (raw):">
+                    <RawRscPayload
+                      rscPayload={currentVersion?.rscPayload ?? ""}
+                    />
+                  </Box>
+                </>
+              ) : null}
             </div>
           </>
         ) : null}
@@ -260,7 +316,7 @@ export default function Page() {
               });
             }}
           >
-            <div className="bg-white w-full rounded-lg flex flex-row p-1.5 gap-1.5 items-center">
+            <div className="bg-white dark:bg-slate-900 w-full rounded-lg flex flex-row p-1.5 gap-1.5 items-center">
               <div className="flex flex-col gap-1.5 grow">
                 <input
                   name="prompt"
@@ -289,13 +345,15 @@ export default function Page() {
               <button
                 type="submit"
                 className={`text-white h-full rounded-md p-2 ${
-                  isPending ? "text-gray-200 bg-gray-500" : "bg-black"
+                  isPending
+                    ? "text-gray-200 bg-gray-500 dark:bg-slate-800"
+                    : "bg-black dark:bg-slate-800"
                 }`}
                 disabled={isPending}
               >
                 {isPending ? (
                   <div className="p-2">
-                    <BeatLoading count={3} />
+                    <BeatLoading count={3} fill="white" />
                   </div>
                 ) : (
                   "Generate"
@@ -344,7 +402,7 @@ function SuggestionButton({
 }) {
   return (
     <button
-      className={`rounded-full bg-white py-1.5 px-3 ${
+      className={`rounded-full bg-white dark:bg-slate-900 py-1.5 px-3 ${
         isPending ? "text-gray-500" : ""
       }`}
       disabled={isPending}
@@ -370,7 +428,12 @@ function ShrinkPreview({ children }: { children: ReactNode }) {
 function PreparingEmptyState() {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <BeatLoading count={3} />
+      <div className="dark:hidden">
+        <BeatLoading count={3} />
+      </div>
+      <div className="hidden dark:block">
+        <BeatLoading count={3} fill="white" />
+      </div>
     </div>
   );
 }
@@ -400,7 +463,7 @@ function Box({
     <section className="flex flex-col gap-1.5">
       <h2 className="font-medium">{title}</h2>
       <div
-        className={`bg-white rounded-lg p-4 min-h-64 ${
+        className={`bg-white dark:bg-slate-900 rounded-lg p-4 min-h-64 ${
           isPending ? "bg-opacity-60" : ""
         }`}
       >
